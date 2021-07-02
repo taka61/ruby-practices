9c3fbab187b87e9261e70d6548dca8b787ba7aea
taka61 <taka.tennisloveup61@gmail.com>
2021-07-02 17:08:35 +0900

    third commit

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -13,29 +13,13 @@ last_day = Date.new(year, month, -1)
 
 puts first_day.strftime("%B %Y").center(21)
 puts " Su Mo Tu We Th Fr St"
-
-case
-when  first_day.wday == 0
-  print "\s" + "　" * first_day.wday
-when  first_day.wday == 1
-  print "\s\s" + "　" * first_day.wday
-when  first_day.wday == 2
-  print "\s\s\s" + "　" * first_day.wday
-when  first_day.wday == 3
-  print "\s\s\s\s" + "　" * first_day.wday
-when  first_day.wday == 4
-  print "\s\s\s\s\s" + "　" * first_day.wday
-when  first_day.wday == 5
-  print "\s\s\s\s\s\s" + "　" * first_day.wday
-when  first_day.wday == 6
-  print "\s\s\s\s\s\s\s" + "　" * first_day.wday
-end    
+print "\s\s\s" * first_day.wday + "\s"
 
 (first_day..last_day).each do |x|
   print  "#{x.day}".rjust(2)
   print " "
-    if x.saturday?
-      print  "\n" + (' ')
+  if x.saturday?
+    print  "\n" + (' ')
     end
 end
 
